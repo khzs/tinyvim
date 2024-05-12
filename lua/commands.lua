afd80e6d1b9393de267becdda1d18b4803bd991c
khzs <khzsolt@gmail.com>
2024-05-12 17:58:45 +0200

    My config

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,4 +1,16 @@
 -- mason, write correct names only
 vim.api.nvim_create_user_command("MasonInstallAll", function()
-  vim.cmd "MasonInstall css-lsp html-lsp lua-language-server typescript-language-server stylua prettier"
+  vim.cmd "MasonInstall lua-language-server stylua prettier"
 end, {})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    local called_with = vim.fn.expand("%")
+    if called_with == "." or called_with == "" then
+      vim.cmd("Telescope find_files")
+    end
+    vim.cmd("NvimTreeOpen")
+    vim.cmd("wincmd p") -- Switch back to the previous window aka the editor
+  end,
+})
